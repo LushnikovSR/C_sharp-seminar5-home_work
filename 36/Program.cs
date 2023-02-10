603b149ed5d787d39410d5ad94d25f8fb7873921
Stanislav_Lushnikov <lust48h@gmail.com>
2023-02-10 19:34:46 +0300

    add homework 36

---
+++ 36/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+Console.Write("Введите значение длинны массива: ");
+int lenArray = int.Parse(Console.ReadLine()!);
+int minValueArray = 1;
+int maxValueArray = 100;
+int[] workArray = GetArray(lenArray, minValueArray, maxValueArray);
+Console.Write($"[{String.Join(", ", workArray)}] -> {SumEvenIndexElementArray(workArray)}");
+
+int SumEvenIndexElementArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);
+    }
+    return array;
+}
